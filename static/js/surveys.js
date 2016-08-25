4834417476bbfddd5bdd152765ff884dbc4b96d9
Maria Mendiburo <mariamendiburo@gmail.com>
2016-08-25 00:46:06 +0000

    adding untracked static folder

---
+++ static/js/surveys.js
@@ -0,0 +1,14 @@
+// Build javascript object that has question id's and results
+
+// Create an empty js object
+// $(.selector) grabs all of questions
+// use js .each (takes a function) add each question id as keys and each response as value
+
+// use Ajax post to send the data to a route
+"use strict";
+// Creating a string of the question id's and responses that can be used for an AJAX request
+data = $("#survey").serialize()
+//put off success function until data is posting successfully 
+// $.post(route, data, success)
+// https://api.jquery.com/jQuery.post/
+// render thanks for submitting and then return to homepage
